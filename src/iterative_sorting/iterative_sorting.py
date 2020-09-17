8c2a83bd946d2282dc5895ada3b668a9b33b3051
Your Name <you@example.com>
2020-09-17 16:46:11 -0400

    finish iterative sort and searching

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,38 @@
 # TO-DO: Complete the selection_sort() function below
+# Source Ref - https://www.w3resource.com/python-exercises/python-basic-exercise-91.php
 def selection_sort(arr):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
+    for i in range(0, len(arr) - 1): # we end the array one index short, because we will call it ourselves by adding 1
+        cur_index = i 
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
-
-
+        for j in range(i+1, len(arr)):# Getting the second index after i by adding 1 to it.
+            if arr[j] < arr[i]:
+                smallest_index = j # smallest value of i & j is located at index j
         # TO-DO: swap
         # Your code here
-
+        curr_lowest = arr[smallest_index]
+        arr[smallest_index] = arr[i]
+        arr[i] = curr_lowest
     return arr
+#  0 1 2 3 4 5
+#  6 4 3 1 5 7
+#  4 3 1 5 6 74
 
-
+#  0 1 2 3 4 5
+#  3 4 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    swapped = True
+    while swapped: # keeps iterating while a swap happened
+        swapped = False
+        for i in range(len(arr) - 1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1]
+                swapped = True
 
     return arr
 
